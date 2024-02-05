40cd10adf2e887ad476353a29b701e486f05dec5
Ben-akiva <127337555+Ben-akiva@users.noreply.github.com>
2024-02-05 15:47:54 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -95,7 +95,7 @@ data "aws_iam_policy" "ebs_csi_policy" {
 
 module "irsa-ebs-csi" {
   source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version = "4.7.0"
+  version = "main"
 
   create_role                   = true
   role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
