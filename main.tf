ab835203835747ae43a4fce6011f3c447125ff8f
Ben-akiva <127337555+Ben-akiva@users.noreply.github.com>
2024-02-05 17:05:08 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ module "vpc" {
 }
 
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
+  source = "git::https://gitlab.com/dash-infrastructure.git?ref=main"
   version = "19.15.3"
 
   cluster_name    = local.cluster_name
