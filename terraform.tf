186466677397128039a9f85f89015e003622785a
Ben-akiva <127337555+Ben-akiva@users.noreply.github.com>
2024-02-05 16:15:33 +0200

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -46,7 +46,7 @@ module "vpc" {
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
-  version = "19.15.3"
+  version = "latest"
 
   cluster_name    = local.cluster_name
   cluster_version = "1.28"
