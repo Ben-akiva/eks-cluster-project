a4651333b6fbbd0e95d1c09d221a8c8c30eef98a
Ben-akiva <127337555+Ben-akiva@users.noreply.github.com>
2024-02-05 15:54:52 +0200

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,31 +1,90 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
+  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
 
-terraform {
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
 
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+  map_public_ip_on_launch = true
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.7.0"
-    }
-
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3.5.1"
-    }
-
-    tls = {
-      source  = "hashicorp/tls"
-      version = "~> 4.0.4"
-    }
-
-    cloudinit = {
-      source  = "hashicorp/cloudinit"
-      version = "~> 2.3.2"
-    }
+  public_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/elb"                      = 1
   }
 
-  required_version = "~> 1.3"
+  private_subnet_tags = {
+    "kubernetes.io/cluster/${local.cluster_name}" = "shared"
+    "kubernetes.io/role/internal-elb"             = 1
+  }
+
 }
 
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "19.15.3"
+
+  cluster_name    = local.cluster_name
+  cluster_version = "1.28"
+
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.public_subnets
+  cluster_endpoint_public_access = true
+
+  eks_managed_node_group_defaults = {
+    ami_type = "AL2_x86_64"
+    public_ip = true
+    ssh_key_name = "key-for-public-eks"
+    ssh_public_key = file("~/eks-cluster-project/key-for-public-eks.pem")
+
+
+  }
+
+  eks_managed_node_groups = {
+    one = {
+      name = "node-ben-"
+
+      instance_types = ["t3.medium"]
+
+      min_size     = 1
+      max_size     = 4
+      desired_size = 3
+      public_ip = true
+      ssh_key_name = "key-for-public-eks"
+      ssh_public_key = file("~/eks-cluster-project/key-for-public-eks.pem")
+
+
+
+    }
+
+
+  }
+}
+
+
+# https://aws.amazon.com/blogs/containers/amazon-ebs-csi-driver-is-now-generally-available-in-amazon-eks-add-ons/
+data "aws_iam_policy" "ebs_csi_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+module "irsa-ebs-csi" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version = "4.7.0"
+
+  create_role                   = true
+  role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
+  provider_url                  = module.eks.oidc_provider
+  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+}
+
+resource "aws_eks_addon" "ebs-csi" {
+  cluster_name             = module.eks.cluster_name
+  addon_name               = "aws-ebs-csi-driver"
+  addon_version            = "v1.20.0-eksbuild.1"
+  service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
+  tags = {
+    "eks_addon" = "ebs-csi"
+    "terraform" = "true"
+  }
+}
