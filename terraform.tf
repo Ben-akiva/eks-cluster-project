2d6bdb4fb2ddcad9eec879891076f0cb75b648bf
Ben-akiva <127337555+Ben-akiva@users.noreply.github.com>
2024-02-05 15:57:53 +0200

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,3 +1,27 @@
+# with managed node groups
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
+locals {
+  cluster_name = "ben-eks-project-${random_string.suffix.result}"
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.0.0"
+
+  name = "ben-education-vpc"
+
+  cidr = "10.0.0.0/16"
   azs  = slice(data.aws_availability_zones.available.names, 0, 3)
 
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
